723b0c99d4d410a1ccfb0852918446cc89453790
Dominik Werder <dominik.werder@gmail.com>
2022-01-19 10:44:19 +0100

    Fix for startup script

--- scripts/bsrep.sh
+++ scripts/bsrep.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-PROGRAM_DIR=$(realpath $(pwd)/$(dirname ${0}))
+if [[ "$0" == /* ]]; then
+    PROGRAM_DIR=$(dirname ${0})
+else
+    PROGRAM_DIR=$(realpath $(pwd)/$(dirname ${0}))
+fi
+
 export LD_LIBRARY_PATH="$PROGRAM_DIR"
 $PROGRAM_DIR/bsrep "$@"
